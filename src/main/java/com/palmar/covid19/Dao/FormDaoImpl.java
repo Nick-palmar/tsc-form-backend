d718d3c62be7b192f42157c40393a451bddf9c3d
Nick-palmar <nicolas.palmar8@gmail.com>
2020-08-14 17:44:47 -0400

    update

--- src/main/java/com/palmar/covid19/Dao/FormDaoImpl.java
+++ src/main/java/com/palmar/covid19/Dao/FormDaoImpl.java
@@ -24,6 +24,7 @@ public class FormDaoImpl {
 	
 	public void insertIntoForm(UserForm formData) throws SQLException{
 		System.out.println("Entering insertInto");
+		System.out.println(dataSource);
 		try (Connection connection = dataSource.getConnection();
 			// sql insert into statement with missing values
 			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO form(form_date,name,email,role,age_group,question_1,question_2,question_3,question_4,question_5,question_6,question_7,question_8,question_9,question_10) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);")) {
