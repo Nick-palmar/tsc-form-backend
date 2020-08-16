0f0581b4779788396de3f79ea9afe1af77b8e1a8
Nick-palmar <nicolas.palmar8@gmail.com>
2020-08-15 21:42:51 -0400

    new connection

--- src/main/java/com/palmar/covid19/controller/UserView.java
+++ src/main/java/com/palmar/covid19/controller/UserView.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,13 +25,13 @@ public class UserView {
 	private FormDaoImpl formDaoImpl = new FormDaoImpl();
 	
 	// use @RequestBody to automatically deserialize the JSON and convert it into the userINfo object
-	@RequestMapping("/form")
+	@PostMapping("/form")
 	public String form(@RequestBody UserForm userInfo) throws URISyntaxException { 
 	    
 		
 		// insert object into forms table
 		try {
-			System.out.println(userInfo.getName());
+			System.out.println(userInfo.getDate());
 			formDaoImpl.insertIntoForm(userInfo);
 		} catch (SQLException e) {
 			e.printStackTrace();
