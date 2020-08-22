70f0cb173537593770e992921555118d3793acf0
Nick-palmar <nicolas.palmar8@gmail.com>
2020-08-22 16:47:49 -0400

    update

--- src/main/java/com/palmar/covid19/controller/AdminView.java
+++ src/main/java/com/palmar/covid19/controller/AdminView.java
@@ -30,7 +30,7 @@ public class AdminView {
 //	    SpringApplication.run(AdminView.class, args);
 //	  }
 	
-	@GetMapping("/admin/form")
+	@GetMapping("/admin/find")
 	@ResponseBody
 	public ArrayList<UserForm> admin(@RequestParam(defaultValue="false") boolean isFlagged,
 			@RequestParam (defaultValue="any") String age, 
